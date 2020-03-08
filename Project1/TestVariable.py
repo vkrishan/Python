8f29af739fd1a491d2b7dd4abf6f798a31d9523e
Vaibhav <vaibhavkrishan.03@gmail.com>
2020-03-07 20:36:14 -0800

    Added Strings examples

--- Project1/TestVariable.py
+++ Project1/TestVariable.py
@@ -38,9 +38,32 @@ del a       # delete a variable
 print(40*"*")
 
 # STRINGS
+#Various String operators/operations
+#  Slice -> [] ;;  Slice -> [start:end]  ;;  in  ;; not in  ;;  %  ;;  conacaternation->  +  ;; repeat->  *
+#   replace()  ;;  upper()  ;; lower()  ;;  join()  ;; reversed(object)  ;; split(' ') ;; replace("This","That")
+#
 
+str1= "Learning Python"
+str2= "This is AMERICA"
 
+print(str1[1])
+print(str1[1:10])   # start:end-1
+print("i" in str1)
+print("i" in str1[7:13])
+print("z" not in str2)
 
+print(str1+str2)
+print(str1[0:5]*2)
+
+str3=str2.replace("AMERICA", "INDIA")
+print(str3)
+str4=str2.replace(str2[8:], "ARGENTINA")
+print(str4)
+
+print(str1.upper())
+print(str2.lower())
+
+print(' '.join(reversed(str1)))
 
 
 
